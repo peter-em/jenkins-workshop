7e260427dda3cc159a78485ce195b2fd57f13d90
Marcin Slowiak <marcin.slowiak@sabre.com>
2020-02-20 22:16:47 +0100

    Wrap with try the application main method call

--- simple-backend/src/main/java/com/slowiak/simplebackend/SimpleBackendApplication.java
+++ simple-backend/src/main/java/com/slowiak/simplebackend/SimpleBackendApplication.java
@@ -11,14 +11,14 @@ public class SimpleBackendApplication {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(SimpleBackendApplication.class, args);
-        SalesforceConnector salesforceConnector = context.getBean(SalesforceConnector.class);
 
-        String productName = context.getEnvironment().getProperty("productName");
-        salesforceConnector.getOrderItemPccByProductName(productName)
-                .doOnNext(orderItemPcc -> log.info("ORDER_ITEM_PCC: " + orderItemPcc.getId() + " -> " + orderItemPcc.getProductName() + " -> " + orderItemPcc.getPcc()))
-                .blockFirst();
-
-        context.close();
+        try (context) {
+            SalesforceConnector salesforceConnector = context.getBean(SalesforceConnector.class);
+            String productName = context.getEnvironment().getProperty("productName");
+            salesforceConnector.getOrderItemPccByProductName(productName)
+                    .doOnNext(orderItemPcc -> log.info("ORDER_ITEM_PCC: " + orderItemPcc.getId() + " -> " + orderItemPcc.getProductName() + " -> " + orderItemPcc.getPcc()))
+                    .blockFirst();
+        }
     }
 
 }
