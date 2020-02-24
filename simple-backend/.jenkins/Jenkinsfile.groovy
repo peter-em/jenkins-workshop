97e6468fb0006d73810cd19c10bab4139111842d
Marcin Slowiak <marcin.slowiak@sabre.com>
2020-02-24 22:20:33 +0100

    Step 4: Run application with Java 11

--- simple-backend/.jenkins/Jenkinsfile.groovy
+++ simple-backend/.jenkins/Jenkinsfile.groovy
@@ -18,5 +18,12 @@ pipeline {
                 }
             }
         }
+        stage('Run') {
+            steps {
+                dir('simple-backend/target') {
+                    sh 'java -jar app.jar'
+                }
+            }
+        }
     }
 }
