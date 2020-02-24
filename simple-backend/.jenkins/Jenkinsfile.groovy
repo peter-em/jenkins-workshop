145db38d5451b5fa5fa7b639f40d7f952fd21c48
Marcin Slowiak <marcin.slowiak@sabre.com>
2020-02-24 21:45:02 +0100

    Step 2: Build application with maven

--- simple-backend/.jenkins/Jenkinsfile.groovy
+++ simple-backend/.jenkins/Jenkinsfile.groovy
@@ -1,9 +1,20 @@
 pipeline {
-    agent any
+    agent {
+        docker {
+            image 'adoptopenjdk/maven-openjdk11'
+        }
+    }
     stages {
         stage('Test') {
             steps {
                 sh 'ls -al'
+            }
+        }
+        stage('Build') {
+            steps {
+                dir('simple-backend') {
+                    sh 'mvn clean install'
+                }
             }
         }
     }
