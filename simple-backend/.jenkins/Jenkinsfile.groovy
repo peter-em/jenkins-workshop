0dcd20564a0fa8a50b6b3e3a0902915fdadd6753
MatuszP <piotr.matusz@sabre.com>
2020-03-04 14:46:59 +0100

    Add run parameters

--- simple-backend/.jenkins/Jenkinsfile.groovy
+++ simple-backend/.jenkins/Jenkinsfile.groovy
@@ -5,6 +5,10 @@ pipeline {
             args '-v /root/.m2/root/.m2'
         }
     }
+    parameters {
+        choice(name: 'PROFILE', choices: ['local', 'dev', 'others'], description: 'Pick something pls nigga')
+        string(name: 'PRODUCT_NAME', defaultValue: 'Pjoter', description: 'WHY ARE YOU RUNNING?!')
+    }
     stages {
         stage('Build') {
             steps {
