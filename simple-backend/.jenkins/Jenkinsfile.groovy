b5d72c21948c1c55a49158ee8fc88992085e52ac
MatuszP <piotr.matusz@sabre.com>
2020-03-04 14:25:50 +0100

    Modify spacing

--- simple-backend/.jenkins/Jenkinsfile.groovy
+++ simple-backend/.jenkins/Jenkinsfile.groovy
@@ -1,23 +1,23 @@
 pipeline {
-  agent {
-    docker {
-      image 'adoptopenjdk/maven-openjdk11'
-    }
-  }
-  stages {
-    stage('FIRST STAGE') {
-      steps {
-        // we're gonna do something
-        sh 'ls -l'
-        sh 'pwd'
-      }
-    }
-    stage('Build') {
-      steps {
-        dir('simple-backend') {
-          sh 'mvn clean install'
+    agent {
+        docker {
+            image 'adoptopenjdk/maven-openjdk11'
         }
-      }
     }
-  }
+    stages {
+        stage('FIRST STAGE') {
+            steps {
+                // we're gonna do something
+                sh 'ls -l'
+                sh 'pwd'
+            }
+        }
+        stage('Build') {
+            steps {
+                dir('simple-backend') {
+                    sh 'mvn clean install'
+                }
+            }
+        }
+    }
 }
